0e23edd2c24a129970afc598171eb4a3fa804041
vanujak <vanujakofficial@gmail.com>
2025-01-08 22:29:43 +0530

    I added a logo and created the front page

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={require('./logo1.png')} className="App-logo" alt="logo" />
+        <p>
+          <code>E-Channelling System</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
